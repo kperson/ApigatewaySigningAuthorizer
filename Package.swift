13b39d1575d527ad7c38046faa691bcf29596558
Kelton Person <kelton.person@gmail.com>
2019-10-31 23:49:02 -0500

    updated swift-aws version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "ApigatewaySigningAuthorizer", targets: ["ApigatewaySigningAuthorizer"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMinor(from: "3.1.0")),
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift.git", .upToNextMinor(from: "3.4.0")),
         .package(url: "https://github.com/kperson/swift-lambda-tools.git", .branch("master")),
         .package(url: "https://github.com/MakeAWishFoundation/SwiftyMocky", .upToNextMinor(from: "3.3.3")),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.1.0"))
